b8c45ed9dc0433f07711a0ad781fe608df0b64ab
Zoltan Polgar <pozo@gmx.com>
2024-06-10 00:01:48 +0200

    Add logging to GrabberService

--- api/src/main/kotlin/com/github/pozo/investmentfunds/api/grabber/GrabberService.kt
+++ api/src/main/kotlin/com/github/pozo/investmentfunds/api/grabber/GrabberService.kt
@@ -1,7 +1,9 @@
 package com.github.pozo.investmentfunds.api.grabber
 
 import com.github.pozo.investmentfunds.DataFlowConstants
+import com.github.pozo.investmentfunds.api.audit.AuditInterceptor
 import com.github.pozo.investmentfunds.api.redis.RedisService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.text.SimpleDateFormat
 import java.util.*
@@ -9,10 +11,14 @@ import java.util.*
 @Service
 class GrabberService : GrabberAPI {
 
+    private val logger = LoggerFactory.getLogger(AuditInterceptor::class.java)
+
     private val format = SimpleDateFormat(DataFlowConstants.GRAB_DATA_COMMAND_DATE_FORMAT.field)
 
     override fun trigger() {
         val latestEndDate = RedisService.jedis.get(DataFlowConstants.GRAB_DATA_LATEST_DATE_KEY.field)
+
+        logger.info("Initiating CSV retrieval mechanism via Redis. The last trigger date was $latestEndDate.")
 
         if (latestEndDate == null || latestEndDate.isEmpty()) {
             RedisService.jedis.publish(
