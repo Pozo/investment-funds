30aee7145f0ff45e86005ed1b0a1164cdb6cc162
Zoltan Polgar <pozo@gmx.com>
2024-07-04 00:09:52 +0200

    Use @Scheduled instead of an endpoint

---
+++ src/main/kotlin/com/github/pozo/investmentfunds/api/grabber/GrabberConfig.kt
@@ -0,0 +1,8 @@
+package com.github.pozo.investmentfunds.api.grabber
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+open class GrabberConfig
--- src/main/kotlin/com/github/pozo/investmentfunds/api/grabber/GrabberService.kt
+++ src/main/kotlin/com/github/pozo/investmentfunds/api/grabber/GrabberService.kt
@@ -5,9 +5,11 @@ import com.github.pozo.investmentfunds.domain.DataFlowConstants
 import org.apache.camel.ProducerTemplate
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 @Service
@@ -19,17 +21,24 @@ class GrabberService constructor(
 
     private val format = SimpleDateFormat(DataFlowConstants.GRAB_DATA_COMMAND_DATE_FORMAT.field)
 
+    @Scheduled(
+        timeUnit = TimeUnit.HOURS,
+        fixedRate = 1,
+        initialDelay = 1
+    )
     override fun trigger() {
         val latestEndDate = RedisService.jedis.get(DataFlowConstants.GRAB_DATA_LATEST_DATE_KEY.field)
 
         logger.info("Initiating CSV retrieval mechanism. The last trigger date was $latestEndDate.")
 
         if (latestEndDate == null || latestEndDate.isEmpty()) {
+            logger.info("Retrieving data from '${DataFlowConstants.START_YEAR_DATE}'")
             producerTemplate.sendBody(
                 "direct:grab-data",
-                "2024.06.01${DataFlowConstants.GRAB_DATA_COMMAND_SEPARATOR.field}${format.format(Date())}"
+                "${DataFlowConstants.START_YEAR_DATE}${DataFlowConstants.GRAB_DATA_COMMAND_SEPARATOR.field}${format.format(Date())}"
             )
         } else {
+            logger.info("Retrieving data from '$latestEndDate'")
             producerTemplate.sendBody(
                 "direct:grab-data",
                 "$latestEndDate,${format.format(Date())}"
