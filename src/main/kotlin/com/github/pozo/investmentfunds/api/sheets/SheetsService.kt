5d4a92ca777d4b5533e682a24da1119f010d8ebf
Zoltan Polgar <pozo@gmx.com>
2024-07-21 14:19:12 +0200

    Return all rates if no filter was given (make things work with CacheService on GAS side)

--- src/main/kotlin/com/github/pozo/investmentfunds/api/sheets/SheetsService.kt
+++ src/main/kotlin/com/github/pozo/investmentfunds/api/sheets/SheetsService.kt
@@ -14,6 +14,21 @@ class SheetsService : SheetsAPI {
     ): List<Map<String, String>> {
         val attribute = filter.attribute ?: RateHeaders.RATE.name.lowercase()
 
+        if(filter.endDate == null && filter.startDate == null && filter.attribute == null) {
+            RedisService.jedis.pipelined().use { pipeline ->
+                val results = RedisService.jedis.zrange("rate:keys#$isin", 0, -1).toList()
+                    .map { pipeline.hget(it, RateHeaders.DATE.name.lowercase()) to pipeline.hget(it, attribute) }
+                pipeline.sync()
+                return results.stream()
+                    .map {
+                        mapOf<String, String>(
+                            RateHeaders.DATE.name.lowercase() to it.first.get(),
+                            attribute to it.second.get()
+                        )
+                    }
+                    .toList()
+            }
+        }
         if (filter.startDate != null) {
             val fromKey = RedisHashKey.calculateScore(filter.startDate)
             val toKey = if(filter.endDate != null) {
@@ -42,7 +57,6 @@ class SheetsService : SheetsAPI {
             RedisService.jedis.pipelined().use { pipeline ->
                 val results =
                     RedisService.jedis.zrevrangeByScore("rate:keys#$isin", Double.MAX_VALUE, Double.MIN_VALUE, 0, 1)
-                        .toList()
                         .map { pipeline.hget(it, RateHeaders.DATE.name.lowercase()) to pipeline.hget(it, attribute) }
                 pipeline.sync()
 
